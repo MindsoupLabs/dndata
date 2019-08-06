2b2364cc6fc3dc6b8453f57da559e165791bb378
Valentijn <valentijn@mindsoup.net>
2019-08-07 00:26:20 +0200

    added publish and status controllers

---
+++ src/main/java/net/mindsoup/dndata/controllers/PublishController.java
@@ -0,0 +1,13 @@
+package net.mindsoup.dndata.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Valentijn on 3-8-2019
+ */
+@RestController
+@RequestMapping(value = "/publish", method = RequestMethod.POST)
+public class PublishController extends ErrorController {
+}
