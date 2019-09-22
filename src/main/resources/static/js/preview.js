fccb9960fda866ef48f10d64152c5257e11e1102
v.geirnaert <VG2016emakina>
2019-09-22 23:02:30 +0200

    added support for lists to preview.js
    (cherry picked from commit 876c7fe4bd5402c986d2420828566cc51e2a6746)

--- src/main/resources/static/js/preview.js
+++ src/main/resources/static/js/preview.js
@@ -1,13 +1,13 @@
-window.onload = function() {
+$(document).ready(function() {
 	$('.js-preview-container').each(function() {
 		$(this).change(function() {
-			renderPreview($(this));
+			initialisePreview($(this));
 		});
-		renderPreview($(this));
+		initialisePreview($(this));
 	});
-};
+});
 
-function renderPreview(element) {
+function initialisePreview(element) {
 	try {
 		var jsonData = $(element).data('preview-json');
 
@@ -19,13 +19,70 @@ function renderPreview(element) {
 			jsonData = JSON.parse(jsonData);
 		}
 
-		$(element).find(".js-preview").each(function(index, element) {
-			var previewKey = $(element).data('preview-key');
-			$(element).html(getDataByPath(previewKey, jsonData));
-		});
+		var data = [];
+		data.push(jsonData);
+
+		renderPreview(element, data, null);
+
 	} catch (error) {
 		console.log(error);
 	}
+}
+
+function renderPreview(parent, data, parentListElement) {
+
+	// for each preview element
+	// simply verify we're not finding one too deep in the hierarchy
+	// then fetch the correct data and append it
+	$(parent).find(".js-preview").each(function(index, element) {
+		var parentElement = $(element).closest('.js-preview-list');
+		if(parentElement.length != 0 && parentElement[0] != parentListElement) {
+			return;
+		}
+
+		$(element).html(getDataForElement(element, data));
+	});
+
+
+	// for each list element
+	$(parent).find(".js-preview-list").each(function(index, element) {
+		// first verify we're not finding an element too deep in the hierarchy
+		var parentElement = $(element).parent().closest('.js-preview-list');
+		if(parentElement.length != 0 && parentListElement != null && parentElement[0] != parentListElement) {
+			return;
+		}
+
+		// get the list data for this element
+		var listData = getDataForElement(element, data);
+		if(listData == null) {
+			return;
+		}
+
+		// store its current child (it should only ever have 1) and then empty it
+		var childElement = $(element).children()[0];
+		$(element).empty();
+
+		// for each item in the list, append a child clone
+		// then render the child with the correct data from the list
+		for(listItem of listData) {
+			var newData = shallowCloneArray(data);
+			var childClone = $(childElement).clone();
+			newData.push(listItem);
+			$(element).append(childClone);
+			renderPreview(childClone, newData, element);
+		}
+	});
+
+}
+
+function shallowCloneArray(array) {
+	var newArray = [];
+
+	for(item of array) {
+		newArray.push(item);
+	}
+
+	return newArray;
 }
 
 function getDataByPath(path, data) {
@@ -35,6 +92,26 @@ function getDataByPath(path, data) {
 		return (getDataByPath(paths.slice(1).join('.'), data[paths[0]]));
 	}
 
+	if(paths[0] == "") {
+		return data;
+	}
+
 	return data[paths[0]];
 }
 
+function getDataForElement(element, data) {
+	var previewKey = $(element).data('preview-key');
+	var parentLevel = $(element).data('preview-parentLevel') || 0;
+
+	// our array is actually most senior data level first, even though we're treating it as most junior data level first
+	// so we must invert the index
+	parentLevel = data.length - 1 - parentLevel;
+
+	if(parentLevel < 0) {
+		console.warn('Warning! Parent level too high for element:');
+		console.warn(element);
+		return {};
+	}
+
+	return getDataByPath(previewKey, data[parentLevel]);
+}
