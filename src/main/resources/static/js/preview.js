0c4793a92e01f74e82970f19acf0e10d4a65e1f8
Valentijn <valentijn@mindsoup.net>
2019-09-23 23:21:23 +0200

    implemented some preview lists, updated some styling

--- src/main/resources/static/js/preview.js
+++ src/main/resources/static/js/preview.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+	$(".js-preview-container").data('preview-json', $("#jsonInput").val());
 	$('.js-preview-container').each(function() {
 		$(this).change(function() {
 			initialisePreview($(this));
@@ -32,14 +33,17 @@ function initialisePreview(element) {
 function renderPreview(parent, data, parentListElement) {
 
 	// for each preview element
-	// simply verify we're not finding one too deep in the hierarchy
-	// then fetch the correct data and append it
 	$(parent).find(".js-preview").each(function(index, element) {
+		// clear each preview element
+		$(element).empty();
+
+		// verify we're not finding one too deep in the hierarchy
 		var parentElement = $(element).closest('.js-preview-list');
 		if(parentElement.length != 0 && parentElement[0] != parentListElement) {
 			return;
 		}
 
+		// fetch the correct data and append it
 		$(element).html(getDataForElement(element, data));
 	});
 
