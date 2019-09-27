140fa3f79c76d612f488fb1b8dc51af1b53b3d35
v.geirnaert <v.geirnaert@emakina.nl>
2019-09-27 13:05:16 +0200

    made edit preview sticky when scrolling

---
+++ src/main/resources/static/js/stickypreview.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+    $(window).on('scroll', function() {
+        $('.js-preview-container').each(function(index, element) {
+            if(elementShouldSticky()) {
+                sticky(element);
+            } else {
+                unsticky(element);
+            }
+        });
+    });
+};
+
+function elementShouldSticky() {
+    var triggerElement = $("#js-preview-sticky-trigger");
+
+    if(triggerElement.length != 1) {
+        return true;
+    }
+
+    return $(triggerElement).offset().top - $(window).scrollTop() < 0;
+}
+
+function sticky(element) {
+    element = $(element);
+    element.css('width', element.width());
+    element.css('top', 10);
+    element.css('z-index', 9001);
+    element.addClass('sticky');
+}
+
+function unsticky(element) {
+    element = $(element);
+    element.removeClass('sticky');
+    element.css('width', '');
+    element.css('top', '');
+    element.css('z-index', '');
+}
