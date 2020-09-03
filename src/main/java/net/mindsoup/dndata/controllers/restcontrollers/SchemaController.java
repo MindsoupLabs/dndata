4661c1336a69cebae24efdb499bd0996d1e07f06
Valentijn <valentijn@mindsoup.net>
2020-09-04 00:47:44 +0200

    updated schema IDs

--- src/main/java/net/mindsoup/dndata/controllers/restcontrollers/SchemaController.java
+++ src/main/java/net/mindsoup/dndata/controllers/restcontrollers/SchemaController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
