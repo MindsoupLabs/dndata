1bb2c451c535fc17c0d5a00d49d9e0fc600716fe
Valentijn <valentijn@mindsoup.net>
2020-09-06 16:23:56 +0200

    added new dash character to sanitation

--- src/main/resources/static/react/JSONFormCreator.js
+++ src/main/resources/static/react/JSONFormCreator.js
@@ -63,7 +63,7 @@ function init() {
 
 function sanitizeJson(json) {
 	// sanitise json that might be copy pasted from source books and contains unusual characters
-	return json.replace(/’/g, "'").replace(/×/g, "x").replace(/–/g, "-").replace(/“/g, "\\\"").replace(/”/g, "\\\"");
+	return json.replace(/’/g, "'").replace(/×/g, "x").replace(/–/g, "-").replace(/—/g, "-").replace(/“/g, "\\\"").replace(/”/g, "\\\"");
 }
 
 // script entry
